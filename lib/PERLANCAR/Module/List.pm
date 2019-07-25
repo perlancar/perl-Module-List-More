3d8c3713868652a038643ecb454d457da8b38b1e
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-07-25 15:23:11 +0700

    Reformat with 4-space indent, just like Module::List 0.004 now

--- lib/PERLANCAR/Module/List.pm
+++ lib/PERLANCAR/Module/List.pm
@@ -9,75 +9,75 @@ use warnings;
 #END IFUNBUILT
 
 sub list_modules($$) {
-	my($prefix, $options) = @_;
-	my $trivial_syntax = $options->{trivial_syntax};
-	my($root_leaf_rx, $root_notleaf_rx);
-	my($notroot_leaf_rx, $notroot_notleaf_rx);
-	if($trivial_syntax) {
-		$root_leaf_rx = $notroot_leaf_rx = qr#:?(?:[^/:]+:)*[^/:]+:?#;
-		$root_notleaf_rx = $notroot_notleaf_rx =
-			qr#:?(?:[^/:]+:)*[^/:]+#;
-	} else {
-		$root_leaf_rx = $root_notleaf_rx = qr/[a-zA-Z_][0-9a-zA-Z_]*/;
-		$notroot_leaf_rx = $notroot_notleaf_rx = qr/[0-9a-zA-Z_]+/;
-	}
-	die "bad module name prefix `$prefix'"
-		unless $prefix =~ /\A(?:${root_notleaf_rx}::
-					 (?:${notroot_notleaf_rx}::)*)?\z/x &&
-			 $prefix !~ /(?:\A|[^:]::)\.\.?::/;
-	my $list_modules = $options->{list_modules};
-	my $list_prefixes = $options->{list_prefixes};
-	my $list_pod = $options->{list_pod};
-	my $use_pod_dir = $options->{use_pod_dir};
-	return {} unless $list_modules || $list_prefixes || $list_pod;
-	my $recurse = $options->{recurse};
-	my $return_path = $options->{return_path};
-	my $all = $options->{all};
-	my @prefixes = ($prefix);
-	my %seen_prefixes;
-	my %results;
-	while(@prefixes) {
-		my $prefix = pop(@prefixes);
-		my @dir_suffix = split(/::/, $prefix);
-		my $module_rx =
-			$prefix eq "" ? $root_leaf_rx : $notroot_leaf_rx;
-		my $pm_rx = qr/\A($module_rx)\.pmc?\z/;
-		my $pod_rx = qr/\A($module_rx)\.pod\z/;
-		my $dir_rx =
-			$prefix eq "" ? $root_notleaf_rx : $notroot_notleaf_rx;
-		$dir_rx = qr/\A$dir_rx\z/;
-		foreach my $incdir (@INC) {
-			my $dir = join("/", $incdir, @dir_suffix);
-			opendir(my $dh, $dir) or next;
-			while(defined(my $entry = readdir($dh))) {
-				if(($list_modules && $entry =~ $pm_rx) ||
-						($list_pod &&
-							$entry =~ $pod_rx)) {
-                                            $results{$prefix.$1} = $return_path ? ($all ? [@{ $results{$prefix.$1} || [] }, "$dir/$entry"] : "$dir/$entry") : undef
-						if $all && $return_path || !exists($results{$prefix.$1});
-				} elsif(($list_prefixes || $recurse) &&
-						($entry ne '.' && $entry ne '..') &&
-						$entry =~ $dir_rx &&
-						-d join("/", $dir,
-							$entry)) {
-					my $newpfx = $prefix.$entry."::";
-					next if exists $seen_prefixes{$newpfx};
-					$results{$newpfx} = $return_path ? ($all ? [@{ $results{$newpfx} || [] }, "$dir/$entry/"] : "$dir/$entry/") : undef
-						if ($all && $return_path || !exists($results{$newpfx})) && $list_prefixes;
-					push @prefixes, $newpfx if $recurse;
-				}
-			}
-			next unless $list_pod && $use_pod_dir;
-			$dir = join("/", $dir, "pod");
-			opendir($dh, $dir) or next;
-			while(defined(my $entry = readdir($dh))) {
-				if($entry =~ $pod_rx) {
-					$results{$prefix.$1} = $return_path ? ($all ? [@{ $results{$prefix.$1} || [] }, "$dir/$entry"] : "$dir/$entry") : undef;
-				}
-			}
-		}
-	}
-	return \%results;
+    my($prefix, $options) = @_;
+    my $trivial_syntax = $options->{trivial_syntax};
+    my($root_leaf_rx, $root_notleaf_rx);
+    my($notroot_leaf_rx, $notroot_notleaf_rx);
+    if($trivial_syntax) {
+        $root_leaf_rx = $notroot_leaf_rx = qr#:?(?:[^/:]+:)*[^/:]+:?#;
+        $root_notleaf_rx = $notroot_notleaf_rx =
+            qr#:?(?:[^/:]+:)*[^/:]+#;
+    } else {
+        $root_leaf_rx = $root_notleaf_rx = qr/[a-zA-Z_][0-9a-zA-Z_]*/;
+        $notroot_leaf_rx = $notroot_notleaf_rx = qr/[0-9a-zA-Z_]+/;
+    }
+    die "bad module name prefix `$prefix'"
+        unless $prefix =~ /\A(?:${root_notleaf_rx}::
+                               (?:${notroot_notleaf_rx}::)*)?\z/x &&
+                                   $prefix !~ /(?:\A|[^:]::)\.\.?::/;
+    my $list_modules = $options->{list_modules};
+    my $list_prefixes = $options->{list_prefixes};
+    my $list_pod = $options->{list_pod};
+    my $use_pod_dir = $options->{use_pod_dir};
+    return {} unless $list_modules || $list_prefixes || $list_pod;
+    my $recurse = $options->{recurse};
+    my $return_path = $options->{return_path};
+    my $all = $options->{all};
+    my @prefixes = ($prefix);
+    my %seen_prefixes;
+    my %results;
+    while(@prefixes) {
+        my $prefix = pop(@prefixes);
+        my @dir_suffix = split(/::/, $prefix);
+        my $module_rx =
+            $prefix eq "" ? $root_leaf_rx : $notroot_leaf_rx;
+        my $pm_rx = qr/\A($module_rx)\.pmc?\z/;
+        my $pod_rx = qr/\A($module_rx)\.pod\z/;
+        my $dir_rx =
+            $prefix eq "" ? $root_notleaf_rx : $notroot_notleaf_rx;
+        $dir_rx = qr/\A$dir_rx\z/;
+        foreach my $incdir (@INC) {
+            my $dir = join("/", $incdir, @dir_suffix);
+            opendir(my $dh, $dir) or next;
+            while(defined(my $entry = readdir($dh))) {
+                if(($list_modules && $entry =~ $pm_rx) ||
+                       ($list_pod &&
+                        $entry =~ $pod_rx)) {
+                    $results{$prefix.$1} = $return_path ? ($all ? [@{ $results{$prefix.$1} || [] }, "$dir/$entry"] : "$dir/$entry") : undef
+                        if $all && $return_path || !exists($results{$prefix.$1});
+                } elsif(($list_prefixes || $recurse) &&
+                            ($entry ne '.' && $entry ne '..') &&
+                            $entry =~ $dir_rx &&
+                            -d join("/", $dir,
+                                    $entry)) {
+                    my $newpfx = $prefix.$entry."::";
+                    next if exists $seen_prefixes{$newpfx};
+                    $results{$newpfx} = $return_path ? ($all ? [@{ $results{$newpfx} || [] }, "$dir/$entry/"] : "$dir/$entry/") : undef
+                        if ($all && $return_path || !exists($results{$newpfx})) && $list_prefixes;
+                    push @prefixes, $newpfx if $recurse;
+                }
+            }
+            next unless $list_pod && $use_pod_dir;
+            $dir = join("/", $dir, "pod");
+            opendir($dh, $dir) or next;
+            while(defined(my $entry = readdir($dh))) {
+                if($entry =~ $pod_rx) {
+                    $results{$prefix.$1} = $return_path ? ($all ? [@{ $results{$prefix.$1} || [] }, "$dir/$entry"] : "$dir/$entry") : undef;
+                }
+            }
+        }
+    }
+    return \%results;
 }
 
 1;
